7f8b60050310df9b9cbf26c303ede2130f0e3add
Kaumon Aung <ag.kaumon@gmail.com>
2024-03-12 10:01:32 +0100

    05_js-backend added

---
+++ 05_js-backend/Unterricht/tag_01/03_package-json/index.js
@@ -0,0 +1,27 @@
+/*
+======================================
+======= package.json erstellen =======
+======================================
+
+Im Terminal:
+"npm init" => erstellt uns eine package.json
+ODER
+"npm init -y" erstellt uns eine package.json mit dem Default Werten
+
+JSON (JavaScript Object Notation)
+
+Was die package.json enhalten kann:
+- name
+- version
+- description
+- main (entry point)
+- test command
+- git repository
+- keywords
+- author
+- license
+- scripts
+- dependencies
+- devDependencies
+- type (module oder commonjs)
+*/
