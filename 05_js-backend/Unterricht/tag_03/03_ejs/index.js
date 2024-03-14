5e58cf900d2b6aba4516213ca73f19e018ae42c5
Kaumon Aung <ag.kaumon@gmail.com>
2024-03-14 11:18:06 +0100

    Tag 03: EJS Einheit

---
+++ 05_js-backend/Unterricht/tag_03/03_ejs/index.js
@@ -0,0 +1,40 @@
+/*
+Um EJS zu installieren, folgenden Befehl im Terminal eingeben:
+
+npm install ejs
+
+app.set(key, value)
+
+key: Der Name von der Einstellung
+value: Der Wert der Einstellung
+
+app.set('view engine', 'ejs')
+app.set('port', 3000)
+app.set('env', 'development')
+*/
+
+const express = require('express');
+const app = express();
+
+// View Engine setzen, die bestimmt wie HTML generiert wird
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index', {
+    title: 'EJS Template',
+    message: 'Die Message wird dynamisch durch EJS dargestellt.',
+    user: {
+      name: 'Max',
+      age: 30,
+    },
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About EJS',
+    htmlContent: '<code>Das ist ein Paragraph aus app.get("/about")</code>',
+  });
+});
+
+app.listen(3000);
---
+++ 05_js-backend/Unterricht/tag_03/03_ejs/notizen.md
@@ -0,0 +1,13 @@
+## Extensions
+
+- EJS language support für Syntax-Highlighting
+
+## EJS Syntax
+
+<% ... %>: Jeglicher JavaScript-Code wird ausgeführt. Wird genutzt für Loops, If Statements (Conditionals)
+
+<%= ... %>: Zeigt JavaScript-Expression in HTML an. Wird genutzt, um dynamischen Content in HTML darzustellen.
+
+<%- ... %>: Ähnlich zu <%= ... %>, aber die JavaScript-Expression wird als reines HTML geparsed. Wird genutzt um HTML Markup darzustellen.
+
+<%# ... %>: Kommentare in EJS. Wird nicht in HTML generiert.
